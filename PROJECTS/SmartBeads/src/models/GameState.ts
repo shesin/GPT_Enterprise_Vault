5708f83aad779e9c4d5ac4e1fb8921fc99e68125
Shekhar <shesin@gmail.com>
2026-07-15 22:17:02 +0530

    Create SmartBeads core engine foundation

--- PROJECTS/SmartBeads/src/models/GameState.ts
+++ PROJECTS/SmartBeads/src/models/GameState.ts
@@ -1,15 +1,1 @@
-export type Player = "RED" | "BLUE";
-
-export interface GameState {
-
-    board: number[];
-
-    currentPlayer: Player;
-
-    moveCount: number;
-
-    winner: Player | null;
-
-    gameOver: boolean;
-
-}
+[]
