46478a8d33763420582d4729b886b23173e8aa11
Shekhar <shesin@gmail.com>
2026-07-18 13:19:12 +0530

    Create SmartBeads board definition structure

---
+++ PROJECTS/SmartBeads/src/boards/Board7.ts
@@ -0,0 +1,6 @@
+import { BoardDefinition } from "../models/GameState"; 
+    export const Board7: BoardDefinition = { 
+      name: "SmartBeads-7", 
+      intersections: [], 
+      connections: [] 
+    };
--- PROJECTS/SmartBeads/src/models/GameState.ts
+++ PROJECTS/SmartBeads/src/models/GameState.ts
@@ -1,9 +1,27 @@
 export type Player = "RED" | "BLUE";
-    
-    export interface GameState {
-      board: number[][];
-      currentPlayer: Player;
-      moveCount: number;
-      winner?: Player;
-      gameOver: boolean;
-    }
+
+export interface Intersection {
+    id: number;
+    occupant?: Player;
+}
+
+export interface Connection {
+    from: number;
+    to: number;
+}
+
+export interface BoardDefinition {
+    name: string;
+    intersections: Intersection[];
+    connections: Connection[];
+}
+
+export interface GameState {
+    board: BoardDefinition;
+
+    currentPlayer: Player;
+    moveCount: number;
+
+    winner?: Player;
+    gameOver: boolean;
+}
